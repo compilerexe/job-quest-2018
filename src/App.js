5364ae5b31f0e7829d07cb59fa4d2c9ee3f52130
Compiler Exe <compilerexe@gmail.com>
2018-06-14 18:51:13 +0700

    add feature random jokes.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Footer from './components/Footer.jsx'
 
 const axios = require('axios')
 
+let firstName = 'Prayut'
+let lastName = ''
+let resultJokes = 20
+
 class App extends Component {
 
   constructor (props) {
@@ -15,10 +19,13 @@ class App extends Component {
     }
   }
 
-  componentDidMount () {
+  fetchData () {
+
+    (this.state.jokes.length > 0) && this.setState({jokes: []})
+
     let result = []
 
-    axios.get('http://api.icndb.com/jokes/random/20?firstName=Prayut&lastName=')
+    axios.get(`http://api.icndb.com/jokes/random/${resultJokes}?firstName=${firstName}&lastName=${lastName}`)
       .then(response => {
         let v = response.data.value
 
@@ -28,13 +35,21 @@ class App extends Component {
 
         this.setState({jokes: result})
       })
+  }
 
+  onSubmit (e) {
+    e.preventDefault()
+    this.fetchData()
+  }
+
+  componentDidMount () {
+    this.fetchData()
   }
 
   render () {
 
     const AppLoading = () => (
-      <div className="columns fixed centered" style={{ top: '50%', left: '50%'}}>
+      <div className="columns fixed centered" style={{top: '50%', left: '50%'}}>
         <div className="loading loading-lg"/>
       </div>
     )
@@ -46,26 +61,30 @@ class App extends Component {
         <div className="columns">
           <div className="column col-2 col-xs-6">
             <div className="form-group">
-              <input type="text" className="form-input" placeholder="First name"/>
+              <input type="text" className="form-input" placeholder="First name"
+                     defaultValue={firstName}
+                     onChange={e => firstName = e.target.value}/>
             </div>
           </div>
           <div className="column col-2 col-xs-6">
             <div className="form-group">
-              <input type="text" className="form-input" placeholder="Last name"/>
+              <input type="text" className="form-input" placeholder="Last name"
+                     defaultValue={lastName}
+                     onChange={e => lastName = e.target.value}/>
             </div>
           </div>
           <div className="column col-2 col-xs-6">
             <div className="form-group">
-              <select className="form-select">
-                <option>20 ~ jokes</option>
-                <option>50 ~ jokes</option>
-                <option>100 ~ jokes</option>
+              <select className="form-select" onChange={e => resultJokes = e.target.value}>
+                <option value="20">20 ~ jokes</option>
+                <option value="50">50 ~ jokes</option>
+                <option value="100">100 ~ jokes</option>
                 <option>more then 100</option>
               </select>
             </div>
           </div>
           <div className="column col-2 col-xs-6">
-            <button type="button" className="btn btn-primary" style={{width: '100%'}}>Random</button>
+            <button type="button" className="btn btn-primary" style={{width: '100%'}} onClick={e => this.onSubmit(e)}>Random</button>
           </div>
         </div>
 
